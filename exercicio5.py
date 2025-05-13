c697299e9bb8d15cce85caf47cc0a84e011aec14
TETEUGL <00001125017442sp@al.educacao.sp.gov.br>
2025-05-13 08:21:44 -0300

    Codigo a fins educativo sobre obter dados de alunos

--- exercicio5.py
+++ exercicio5.py
@@ -4,12 +4,13 @@ alunos = []
 
 def obter_dados_aluno():
     nome_aluno = input("Informe seu nome completo: ")
-    idade_aluno = input("Informe sua idade: ")
     email = input("Informe o email do aluno: ")
     serie = input("Informe a serie do aluno: ")
     nota01 = float(input("Informe a primeira nota do aluno"))
     nota02 = float(input("Informe a segunda nota do aluno"))
     nota03 = float(input("Informe a terceira nota do aluno"))
+
+    return cadastrar_aluno(nome_aluno, email, serie, nota01, nota02, nota03)
 
 def cadastrar_aluno(nome, email, serie, nota01=0, nota02=0, nota03=0):
     
